3b01019d288740f7d9013387de111b5a6abdfc81
Paula Solaz Pérez <psolaz@campuscamarafp.com>
2026-02-12 09:47:23 +0100

    no me acuerdo

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -3,12 +3,31 @@ let currentStep = 0;
 const steps = document.querySelectorAll(".step");
 const dots = document.querySelectorAll(".dot");
 
-function showStep(index) {
-  steps.forEach(step => step.classList.remove("active"));
-  dots.forEach(dot => dot.classList.remove("active"));
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
+function showStep(index) {
+  // steps
+  steps.forEach(step => step.classList.remove("active"));
   steps[index].classList.add("active");
+
+  // dots
+  dots.forEach(dot => dot.classList.remove("active"));
   dots[index].classList.add("active");
+
+  // 🔹 BOTÓN ANTERIOR
+  if (index === 0) {
+    prevBtn.style.visibility = "hidden";
+  } else {
+    prevBtn.style.visibility = "visible";
+  }
+
+  // 🔹 BOTÓN SIGUIENTE / FINALIZAR
+  if (index === steps.length - 1) {
+    nextBtn.textContent = "Finalizar";
+  } else {
+    nextBtn.textContent = "Siguiente";
+  }
 }
 
 function nextStep() {
@@ -28,7 +47,7 @@ function prevStep() {
 }
 
 function submitPreferences() {
-  console.log("Formulario completado");
+  window.location.href = "auth/login.html";
 }
 
 showStep(currentStep);
